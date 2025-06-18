6e51f73fe52386ce227a359880f0efb4b67cc8c3
iamseung <seesungseok@naver.com>
2025-06-18 18:44:59 +0900

    ✅ 유저 컨트롤러 생성

---
+++ src/main/java/com/example/blocking_service/dto/ApiSuccessResponse.java
@@ -0,0 +1,32 @@
+package com.example.blocking_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Getter;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ApiSuccessResponse<T> {
+    private final String message;
+    private final T data;
+
+    private ApiSuccessResponse(String message, T data) {
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiSuccessResponse<T> of(String message, T data) {
+        return new ApiSuccessResponse<>(message, data);
+    }
+
+    public static ApiSuccessResponse<Void> message(String message) {
+        return new ApiSuccessResponse<>(message, null);
+    }
+
+    public static ApiSuccessResponse<Void> ok() {
+        return new ApiSuccessResponse<>("OK", null);
+    }
+
+    public static <T> ApiSuccessResponse<T> ok(T data) {
+        return new ApiSuccessResponse<>("OK", data);
+    }
+}
