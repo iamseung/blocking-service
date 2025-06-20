9f038f0561ec64d916044e66eb5f03649d82ddb2
iamseung <seesungseok@naver.com>
2025-06-20 22:57:58 +0900

    ✅ 구조 변경 V2

--- src/main/java/com/example/blocking_service/domain/request/LoginRequest.java
+++ src/main/java/com/example/blocking_service/domain/request/LoginRequest.java
@@ -1,11 +1,10 @@
-package com.example.blocking_service.dto;
+package com.example.blocking_service.domain.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
-
     @NotBlank private String userId;
     @NotBlank private String password;
 }
